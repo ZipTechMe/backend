c62998c0e4caad71b753ffc4273dba775a32294a
JYB\jybeo <jybeomss1@gmail.com>
2025-06-24 15:50:47 +0900

    init

---
+++ src/main/java/com/jybeomss1/realestateauction/RealEstateAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.jybeomss1.realestateauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jybeomss1/realestateauction/RealEstateAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jybeomss1.realestateauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
