8762f82441ab4960e40d4228d8652dd5563fdfa0
JYB\jybeo <jybeomss1@gmail.com>
2025-07-01 15:52:42 +0900

    fix: request noconstructor 추가

--- src/main/java/com/jybeomss1/realestateauction/user/domain/dto/UserJoinRequest.java
+++ src/main/java/com/jybeomss1/realestateauction/user/domain/dto/UserJoinRequest.java
@@ -1,8 +1,10 @@
 package com.jybeomss1.realestateauction.user.domain.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UserJoinRequest {
     private String email;
     private String name;
